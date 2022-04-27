07e1dc03dab1999152d752aff929cfdbd0b72ba3
nazzzzzer <alishereleusinov@gmail.ru>
2022-04-27 16:11:42 +0600

    shop

---
+++ mda/admin.py
@@ -0,0 +1,9 @@
+
+from .models.mod import Category,Product
+from django.contrib import admin
+
+
+admin.site.register(Category)
+admin.site.register(Product)
+
+
---
+++ mda/models/mod.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30, db_index=True)
+    slug = models.SlugField(max_length=10, db_index=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images')
+    description = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.product_name
